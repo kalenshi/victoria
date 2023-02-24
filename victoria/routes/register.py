f1cfb9ce85350d51109ab385cfab8d7eb04f00dc
kalenshi <kalenshi@gmail.com>
2023-02-24 15:16:08 -0400

    Added user profile updating and image resizing

--- victoria/routes/register.py
+++ victoria/routes/register.py
@@ -7,22 +7,22 @@ from victoria.models import User
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    register_form = RegistrationForm()
+    update_form = RegistrationForm()
     if current_user.is_authenticated:
         return redirect(location=url_for("home"))
-    if register_form.validate_on_submit():
+    if update_form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(
-            register_form.password.data
+            update_form.password.data
         ).decode("utf-8")
         # Create the user and encrypt the password
         user = User(
-            first_name=register_form.first_name.data,
-            last_name=register_form.last_name.data,
-            email=register_form.email.data,
+            first_name=update_form.first_name.data,
+            last_name=update_form.last_name.data,
+            email=update_form.email.data,
             password=hashed_password
         )
         db.session.add(user)
         db.session.commit()
-        flash(f"Account created for {register_form.email.data}", category="success")
+        flash(f"Account created for {update_form.email.data}", category="success")
         return redirect(location=url_for("login"))
-    return render_template("auth/register.html", title="Register", register_form=register_form)
+    return render_template("auth/register.html", title="Register", update_form=update_form)
